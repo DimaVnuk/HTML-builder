11bcdd76e835ebbd83cca3c279d125647ea67a9e
DimaVnuk <vnuchok311994@hotmail.com>
2021-11-06 21:36:15 +0300

    feat: add 03-file-in-folder task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,18 @@
+let fs = require("fs");
+const path = require("path");
+
+let secretFolder = path.join(__dirname, "secret-folder");
+
+fs.readdir(secretFolder, (err, files) => {
+  if (err) throw err;
+  files.forEach((i) => {
+    fs.stat(`${secretFolder}/${i}`, (err, stats) => {
+      if (err) throw err;
+      console.log(
+        `${i.split(".").join(" - ")} - ${(stats.size / 1000)
+          .toString()
+          .slice(0, 4)} kb`
+      );
+    });
+  });
+});
